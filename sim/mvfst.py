932b799022fd66329dd0c7359ea5a4712b2b4117
Krishna Sathyamurthy <64264567+krshsl@users.noreply.github.com>
2024-12-05 14:44:25 -0500

    using tempdir and threads for rtt

--- sim/mvfst.py
+++ sim/mvfst.py
@@ -1,8 +1,37 @@
 from src.env import HANDSHAKE, MVFST_PKG, RUN_SIM, SIM_FILE, RTT_ITERS, RTT
+from tempfile import TemporaryDirectory, mkdtemp
 from src.parser import MyParser
-from os import path
+from os import path, walk
 from subprocess import run
 from filecmp import cmp
+from multiprocessing import Pool, cpu_count
+
+def rtt_mult(arg):
+    '''
+        this function handles running rtt across various threads
+        :param arg: (rtt dir, iter, host addr)
+    '''
+    temp_dir = mkdtemp(suffix="__"+str(arg[1]), dir=arg[0].name)
+    sims = ','.join(["/%s"%SIM_FILE]*RTT_ITERS)
+    run([MVFST_PKG, '--mode=client', '--path=%s'%sims, '--outdir=%s'%temp_dir, '--host=%s'%arg[2]], capture_output=True)
+
+def check_mult(arg):
+    '''
+        this function handles verifying rtt file across various threads
+        :param arg: (rtt dir, sim file)
+    '''
+    not_work = []
+
+    sim_file = path.join(arg[0], SIM_FILE)
+    if not path.exists(sim_file) or not cmp(sim_file, arg[1]):
+        not_work.append(sim_file)
+
+    for i in range(1, RTT_ITERS):
+        curr_file = sim_file + '_' + str(i)
+        if not path.exists(curr_file) or not cmp(curr_file, arg[1]):
+            not_work.append(curr_file)
+
+    return not_work
 
 class CLIENT(RUN_SIM):
     def __init__(self, host):
@@ -13,27 +42,43 @@ class CLIENT(RUN_SIM):
         self.multiple()
 
     def handshake(self):
+        '''
+            this function checks for basic handshake
+        '''
         print("Running handshake...")
-        self.hndshk = path.join(self.out_dir, HANDSHAKE)
-        self._rm_mk_dir(self.hndshk)
-        out_file = path.join(self.hndshk, SIM_FILE)
-        run([MVFST_PKG, '--mode=client', '--path=/%s'%SIM_FILE, '--outdir=%s'%self.hndshk, '--host=%s'%self.host])
-        if not cmp(out_file, self.sim_file):
-            print("doesn't work")
+        self.hndshk = TemporaryDirectory(suffix=HANDSHAKE, dir=self.out_dir.name)
+        out_file = path.join(self.hndshk.name, SIM_FILE)
+        run([MVFST_PKG, '--mode=client', '--path=/%s'%SIM_FILE, '--outdir=%s'%self.hndshk.name, '--host=%s'%self.host], capture_output=True)
+        if cmp(out_file, self.sim_file):
+            print("handshake works...")
         else:
-            print("works...")
+            print("handshake doesn't work")
 
     def multiple(self):
+        '''
+            this function loads the serve by sending files concurrently
+        '''
         print("Running rtt...")
-        self.rtt = path.join(self.out_dir, RTT)
-        self._rm_mk_dir(self.rtt)
-        out_file = path.join(self.rtt, SIM_FILE)
-        sims = ','.join(["/"+SIM_FILE]*RTT_ITERS)
-        run([MVFST_PKG, '--mode=client', '--path=%s'%sims, '--outdir=%s'%self.rtt, '--host=%s'%self.host])
-        if not cmp(out_file, self.sim_file):
-            print("doesn't work")
+        self.rtt = TemporaryDirectory(suffix=RTT, dir=self.out_dir.name)
+        params = [(self.rtt, 0, self.host) for i in range(RTT_ITERS)]
+        with Pool(cpu_count()) as p:
+            p.map(rtt_mult, params)
+
+        print("Verifying rtt...")
+        params = [(x[0], self.sim_file) for x in walk(self.rtt.name, topdown=False)]
+        params.pop()
+        not_work = []
+        with Pool(cpu_count()) as p:
+            failures = p.map(check_mult, params)
+            for fail in failures:
+                if len(fail):
+                    not_work.extend(fail)
+
+        if len(not_work):
+            print(not_work)
         else:
-            print("works...")
+            print("Rtt seems to work fine...")
+
 
 class SERVER(RUN_SIM):
     def __init__(self, host):
@@ -41,7 +86,7 @@ class SERVER(RUN_SIM):
 
     def collect_data(self):
         # keep running the background or whatever...
-        run([MVFST_PKG, '--mode=server', '-static_root=%s'%self.sim_dir, '--host=%s'%self.host])
+        run([MVFST_PKG, '--mode=server', '-static_root=%s'%self.sim_dir.name, '--host=%s'%self.host])
 
 if __name__ == "__main__":
     parser = MyParser(description = "Run sim suite to collect data")
--- sim/src/env.py
+++ sim/src/env.py
@@ -1,16 +1,17 @@
-from os import path, makedirs
+from tempfile import TemporaryDirectory
+from os import path
 from urllib.request import urlopen
-from shutil import rmtree
+
 
 MVFST_PKG :str = "/vcpkg/packages/proxygen_x64-linux/tools/proxygen/hq"
 CHROME_PKG :str = ""
-SIM_DIR :str = "./quic"
-SIM_FILE :str = "index.html"
+SIM_DIR :str = "quic"
+SIM_FILE :str = "index.html" # ~ 1kb
 SIM_URL :str = "www.example.org"
 OUT_DIR :str = "out"
 HANDSHAKE: str = "handshake"
 RTT: str = "rtt"
-RTT_ITERS: int = 50
+RTT_ITERS: int = 128
 
 class RUN_SIM:
     def __init__(self, host):
@@ -18,23 +19,14 @@ class RUN_SIM:
         self._create_root_folder()
         self._download_files()
 
-    def _rm_mk_dir(self, dir):
-        if path.exists(dir):
-            rmtree(dir)
-
-        makedirs(dir)
-
     def _create_root_folder(self):
-        self.sim_dir = path.join(SIM_DIR, SIM_URL)
-        self._rm_mk_dir(self.sim_dir)
-
-        self.out_dir = path.join(SIM_DIR, OUT_DIR)
-        self._rm_mk_dir(self.out_dir)
+        self.sim_dir = TemporaryDirectory(prefix=SIM_DIR)
+        self.out_dir = TemporaryDirectory(prefix=OUT_DIR, dir=self.sim_dir.name)
 
     def _download_files(self):
         with urlopen("http://"+SIM_URL) as webpage:
             content = webpage.read().decode()
 
-        self.sim_file = path.join(self.sim_dir, SIM_FILE)
+        self.sim_file = path.join(self.sim_dir.name, SIM_FILE)
         with open(self.sim_file, 'w') as output:
             output.write(content)
