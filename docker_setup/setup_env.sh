4b702a3d1677be25192669555eb35f0f10641ef3
Krishna Sathyamurthy <64264567+krshsl@users.noreply.github.com>
2024-12-11 14:35:32 -0500

    modularized init setup and fixed errors

--- docker_setup/setup_env.sh
+++ docker_setup/setup_env.sh
@@ -1,3 +1,36 @@
+#!/bin/bash
+
+test_envs="mvfst quic"
+docker_run="docker run -it --entrypoint=/bin/bash -u root --cap-add=NET_ADMIN --cap-add=NET_RAW --privileged -v ./../../sim:/sim -v /filer/tmp1/$USER/mvfst:/sim/mvfst  -v /filer/tmp1/$USER/quic:/sim/quic"
+
+function quic {
+    cd chromium
+    chk_image="quic:cn"
+    image_name="$(docker ps -a | grep -m 1 $chk_image | awk '{print $2;}')"
+    if [ "$image_name" != "$chk_image" ]; then
+        volume="-v ./chromium:/chromium -v ./depot_tools:/depot_tools"
+        $docker_run $volume --name quic_s $chk_image
+        $docker_run $volume --name quic_c $chk_image
+    else
+        echo "chromium already setup"
+    fi
+    cd ..
+}
+
+function mvfst {
+    cd proxygen
+    chk_image="proxygen:cn"
+    image_name="$(docker ps -a | grep -m 1 $chk_image | awk '{print $2;}')"
+    if [ "$image_name" != "$chk_image" ]; then
+        volume="-v ./vcpkg:/vcpkg"
+        $docker_run $volume --name proxygen_s $chk_image
+        $docker_run $volume --name proxygen_c $chk_image
+    else
+        echo "proxygen already setup"
+    fi
+    cd ..
+}
+
 chmod +x chromium/setup_env.sh endpoint/setup_env.sh
 cd endpoint
 ./setup_env.sh
@@ -7,32 +40,27 @@ cd ../proxygen && ./setup_env.sh &
 echo "Waiting..."
 wait
 
-cd ../../sim
-mkdir -p /filer/tmp1/$USER/mvfst
-mkdir -p /filer/tmp1/$USER/quic
-ln -s /filer/tmp1/$USER/mvfst mvfst
-ln -s /filer/tmp1/$USER/quic quic
-
-cd ../docker_setup/chromium
-chk_image="quic:cn"
-docker_run="docker run -it --entrypoint=/bin/bash -u root --cap-add=NET_ADMIN --cap-add=NET_RAW --privileged -v ./../../sim:/sim -v /filer/tmp1/$USER/mvfst:/sim/mvfst  -v /filer/tmp1/$USER/quic:/sim/quic"
-image_name="$(docker ps -a | grep -m 1 $chk_image | awk '{print $2;}')"
-if [ "$image_name" != "$chk_image" ]; then
-  volume='-v ./chromium:/chromium -v ./depot_tools:/depot_tools'
-  $docker_run $volume --name quic_s $chk_image
-  $docker_run $volume --name quic_c $chk_image
-else
-  echo "chromium already setup"
-fi
-cd ../proxygen
-chk_image="proxygen:cn"
-image_name="$(docker ps -a | grep -m 1 $chk_image | awk '{print $2;}')"
-if [ "$image_name" != "$chk_image" ]; then
-  volume="-v ./vcpkg:/vcpkg"
-  $docker_run $volume --name proxygen_s $chk_image
-  $docker_run $volume --name proxygen_c $chk_image
-else
-  echo "proxygen already setup"
-fi
 cd ..
+for test_env in $test_envs; do
+    cd ../sim
+    mkdir -p /filer/tmp1/$USER/$test_env
+    if [ ! -d "$test_env" ]; then
+        ln -s /filer/tmp1/$USER/$test_env $test_env
+    else
+        echo "sym link already setup for $test_env"
+    fi
+
+    cd ../docker_setup
+    while true
+    do
+        echo -n "Do you want to generate interactive $test_env docker containers?"
+        read -r -p '' choice
+        case "$choice" in
+        n|N) break;;
+        y|Y) $test_env; break;;
+        *) echo 'Response not valid';;
+        esac
+    done
+done
+
 echo "Fin..."
